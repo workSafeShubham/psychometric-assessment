9ab91d0c82657b0a6b403324babbbb6d65b6de06
workSafeShubham <workwalashubham@gmail.com>
2025-02-19 11:02:20 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import PsychometricAssessment from '@/components/PsychometricAssessment';
+
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4">
+      <PsychometricAssessment />
+    </main>
+  );
+}
